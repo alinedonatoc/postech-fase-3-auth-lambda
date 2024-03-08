5ebea0b48dfce6b8a4f4db4d1fa9da561ae79c3c
aline.tanure <alinedonatoc@gmail.com>
2024-03-07 21:20:13 -0300

    Adjust format

--- api-gateway.tf
+++ api-gateway.tf
@@ -1,13 +1,20 @@
-resource "aws_api_gateway_rest_api" "api_gateway" {
-  name        = "Minha API"
-  description = "AWS Rest API de exemplo com Terraform"
+# api-gateway.tf
+resource "aws_api_gateway_rest_api" "my_api_gateway" {
+  name        = "My API"
+  description = "AWS Rest API example with Terraform"
   endpoint_configuration {
-   types = ["REGIONAL"]
+    types = ["REGIONAL"]
   }
 }
-resource "aws_api_gateway_authorizer" "authorizer" {
-  name          = "cognito"
-  rest_api_id   = aws_api_gateway_rest_api.api_gateway.id
-  type          = "COGNITO_USER_POOLS"
-  provider_arns = [aws_cognito_user_pool.pool.arn]
+
+resource "aws_api_gateway_resource" "my_api" {
+  rest_api_id = aws_api_gateway_rest_api.my_api_gateway.id
+  parent_id   = aws_api_gateway_rest_api.my_api_gateway.root_resource_id
+  path_part   = "my-api"
+}
+
+resource "aws_api_gateway_resource" "v1" {
+  rest_api_id = aws_api_gateway_rest_api.my_api_gateway.id
+  parent_id   = aws_api_gateway_resource.my_api.id
+  path_part   = "v1"
 }
--- provider.tf
+++ provider.tf
@@ -1,5 +1,4 @@
 provider "aws" {
-    region = "us-east-1"
-    access_key = "my-access-key"
-    secret_key = "my-secret-key"
+  region  = "us-east-1"
+  version = "2.61"
 }
