a8070a19612e27f353f850836dec62d5daa68a06
aline.tanure <alinedonatoc@gmail.com>
2024-03-07 21:32:46 -0300

    Add more resources

--- cognito.tf
+++ cognito.tf
@@ -1,27 +1,24 @@
 resource "aws_cognito_user_pool" "pool" {
-  name = "minha-api-user-pool"
+  name = "mypool"
 }
-resource "aws_cognito_user_pool_domain" "domain" {
-  domain       = "minha-api-serverless-exemplo"
-  user_pool_id = aws_cognito_user_pool.pool.id
-}
-resource "aws_cognito_resource_server" "resource_server" {
-  identifier   = "minha-api"
-  name         = "resource-server-minha-api"
-  user_pool_id = aws_cognito_user_pool.pool.id
-  scope {
-    scope_name        = "sms"
-    scope_description = "Todos os recursos"
-  }
-}
+
 resource "aws_cognito_user_pool_client" "client" {
-  name                                 = "minha-api-client"
-  user_pool_id                         = aws_cognito_user_pool.pool.id
-  generate_secret                      = true
-  explicit_auth_flows                  = ["ALLOW_CUSTOM_AUTH", "ALLOW_REFRESH_TOKEN_AUTH"]
+  name                                 = "client"
   allowed_oauth_flows_user_pool_client = true
-  allowed_oauth_flows                  = ["client_credentials"]
-  allowed_oauth_scopes                 = ["minha-api/sms"]
+  generate_secret                      = false
+  allowed_oauth_scopes                 = ["aws.cognito.signin.user.admin", "email", "openid", "profile"]
+  allowed_oauth_flows                  = ["implicit", "code"]
+  explicit_auth_flows                  = ["ADMIN_NO_SRP_AUTH", "USER_PASSWORD_AUTH"]
   supported_identity_providers         = ["COGNITO"]
-  depends_on                           = [aws_cognito_resource_server.resource_server]
+
+
+  user_pool_id  = aws_cognito_user_pool.pool.id
+  callback_urls = ["https://example.com"]
+  logout_urls   = ["https://sumeet.life"]
+}
+
+resource "aws_cognito_user" "example" {
+  user_pool_id = aws_cognito_user_pool.pool.id
+  username     = "sumeet.n"
+  password     = "Test@123"
 }
--- provider.tf
+++ provider.tf
@@ -1,4 +1,15 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.10.0"
+    }
+  }
+
+}
+
 provider "aws" {
-  region  = "us-east-1"
-  
+  alias  = "aws_useast1"
+  region = "us-east-1"
+
 }
