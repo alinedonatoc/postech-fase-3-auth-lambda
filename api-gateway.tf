b14d24d312bead55ef78ccedd8ac2730ed08852f
aline.tanure <alinedonatoc@gmail.com>
2024-03-07 21:01:54 -0300

    Adjust format

--- api-gateway.tf
+++ api-gateway.tf
@@ -5,3 +5,9 @@ resource "aws_api_gateway_rest_api" "api_gateway" {
    types = ["REGIONAL"]
   }
 }
+resource "aws_api_gateway_authorizer" "authorizer" {
+  name          = "cognito"
+  rest_api_id   = aws_api_gateway_rest_api.api_gateway.id
+  type          = "COGNITO_USER_POOLS"
+  provider_arns = [aws_cognito_user_pool.pool.arn]
+}
