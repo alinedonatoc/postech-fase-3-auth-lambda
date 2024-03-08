2509e8694a2b4d0dc823e80f47522e75cf143b14
aline.tanure <alinedonatoc@gmail.com>
2024-03-08 10:06:59 -0300

    Add more resources

--- apigateway.tf
+++ apigateway.tf
@@ -139,25 +139,3 @@ resource "aws_api_gateway_deployment" "deployment" {
   rest_api_id = aws_api_gateway_rest_api.my_api.id
   stage_name  = "dev"
 }
-
-
-
-# // domain
-resource "aws_acm_certificate" "my_api_cert" {
-  domain_name               = "api.sumeet.life"
-  provider                  = aws
-  subject_alternative_names = ["api.sumeet.life"] # Your custom domain
-  validation_method         = "DNS"
-}
-
-resource "aws_api_gateway_domain_name" "gw_domain" {
-  certificate_arn = aws_acm_certificate.my_api_cert.arn
-  domain_name     = "api.sumeet.life"
-  security_policy = "TLS_1_2"
-}
-
-resource "aws_api_gateway_base_path_mapping" "gw_mapping" {
-  domain_name = "api.sumeet.life"
-  api_id      = aws_api_gateway_rest_api.my_api.id
-  stage_name  = "dev" # Adjust as needed
-}
